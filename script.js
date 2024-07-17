51ed52a7ddad64be9ac53d217d2f463caaec5f8a
Gundapu Venkata Lokesh Karunakar <121012982+8143620747@users.noreply.github.com>
2024-07-17 22:04:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (name === '' || email === '' || message === '') {
+        alert('Please fill in all fields.');
+        return;
+    }
+    else{
+        alert('Details submitted succesfully');
+        return;
+    }
+});
